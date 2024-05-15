b26dee429ecce55a4f64720882a39e0c9c697053
Owen Harvey <owenlharvey@gmail.com>
2024-05-15 11:53:05 +1000

    Starting on message syncing using a "last queried" table

--- src/Data/Types/Message.hs
+++ src/Data/Types/Message.hs
@@ -13,6 +13,7 @@ import Data.Types.Orphans ()
 import Data.Time
 import Data.UUID.V4 (nextRandom)
 import Database.SQLite.Simple.ToField
+import Data.Types.Error
 
 newtype MessageId = MessageId { unMessageId :: UUID }
   deriving (Eq, Ord, Show, Generic)
@@ -43,10 +44,32 @@ instance FromRow Message where
     <*> field
     <*> field
 
-getMessagesForUser :: CanAppM m c e => UserId -> m [Message]
-getMessagesForUser uid = do
+-- Store the last time a user requested their messages.
+data MessageSync = MessageSync
+  { syncUser :: UserId
+  , syncTime :: UTCTime
+  } deriving (Eq, Ord, Show, Generic)
+instance FromRow MessageSync where
+  fromRow = MessageSync <$> field <*> field
+
+getMessageSync :: CanAppM m c e => UserId -> m (Maybe MessageSync)
+getMessageSync uid = do
+  c <- asks conn
+  l <- liftIO $ query c "select user, time from message_sync where user = ?" (Only uid)
+  case l of
+    [] -> pure Nothing
+    [sync] -> pure $ Just sync
+    _ -> throwError_ $ DB $ TooManyResults
+
+getAllMessagesForUser :: CanAppM m c e => UserId -> m [Message]
+getAllMessagesForUser uid = do
   c <- asks conn
   liftIO $ query c "select id, from, to, body, sent from message where from = ?" (Only uid)
+
+getMessagesForUserSince :: CanAppM m c e => UserId -> UTCTime -> m [Message]
+getMessagesForUserSince uid since = do
+  c <- asks conn
+  liftIO $ query c "select id, from, to, body, sent from message where from = ? and sent >= ?" (uid, since)
 
 writeMessage :: CanAppM m c e => CreateMessage -> m MessageId
 writeMessage (CreateMessage from to body) = do
